612fb5b20360120a3c952d35bcbb1a2916e909c9
sahit <chsahit@gmail.com>
2015-01-05 20:32:54 -0500

    built inital class structure, ditched initial classes

---
+++ src/org/usfirst/frc/team11/robot/subsystems/dt/leftDT.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc.team11.robot.subsystems.dt;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class leftDT extends Subsystem {
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
