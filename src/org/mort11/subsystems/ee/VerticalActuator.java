04c6240a220ebce7c056aa9b1ab7730a627a693a
Jakob Shortell <Jakob@Jakobs-Air.MORT>
2015-01-20 19:22:16 -0500

    set elevator motors to a speed

--- src/org/mort11/subsystems/ee/VerticalActuator.java
+++ src/org/mort11/subsystems/ee/VerticalActuator.java
@@ -1,45 +1,48 @@
 package org.mort11.subsystems.ee;
 
+import static org.mort11.util.EEConstants.IRVoltsToInches;
+
 import org.mort11.RobotMap;
 import org.mort11.commands.ee.Elevate;
 
-import static org.mort11.util.EEConstants.*;
 import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
-public class VerticalActuator extends Subsystem 
-{
-    
+public class VerticalActuator extends Subsystem {
+
 	AnalogInput ir = new AnalogInput(RobotMap.IRPort);
 	double height;
-	
+	Talon motor1 = new Talon(1);
+	Talon motor2 = new Talon(2);
+
 	/**
 	 * get height of elevator
+	 * 
 	 * @return displacement of elevator from origin inches
 	 */
-	public double getDisplacementInInches()
-	{
+	public double getDisplacementInInches() {
 		return 0;
 	}
-	public void zero()
-	{
-		
+
+	public void zero() {
+
 	}
-	public double getHeight()
-	{
-		height = ir.getVoltage()*IRVoltsToInches;
+
+	public double getHeight() {
+		height = ir.getVoltage() * IRVoltsToInches;
 		return height;
 	}
-	public void setSpeed(double speed)
-	{
-		
-	}
-    public void initDefaultCommand()
-	{
-       setDefaultCommand(new Elevate());
-    }
-}
 
+	public void setSpeed(double speed) {
+		motor1.set(speed);
+		motor2.set(speed);
+	}
+
+	public void initDefaultCommand() {
+		setDefaultCommand(new Elevate());
+	}
+}
