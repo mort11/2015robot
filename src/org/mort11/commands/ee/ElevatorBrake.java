2318b35e8cc7ae22bd685d4290d520d51e0c9ee0
abi <akoutha7@gmail.com>
2015-01-31 14:07:52 -0500

    created elevator break command and implemented it in elevatortoheight

--- src/org/mort11/commands/ee/ElevatorBrake.java
+++ src/org/mort11/commands/ee/ElevatorBrake.java
@@ -1,16 +1,17 @@
 package org.mort11.commands.ee;
-import static org.mort11.Robot.*;
-
-import org.mort11.Robot;
-import org.mort11.util.*;
 
 import edu.wpi.first.wpilibj.command.Command;
+import org.mort11.subsystems.ee.Brake
 
-public class ElevateToHeight extends Command {
-	int toteheight;
-    public ElevateToHeight(int toteHeight) {
-        this.toteheight = toteHeight;
-        requires(Robot.elevator);
+
+/**
+ *
+ */
+public class ElevatorBrake extends Command {
+
+    public ElevatorBrake() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
     }
 
     // Called just before this Command runs the first time
@@ -19,19 +20,15 @@ public class ElevateToHeight extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	double error = toteheight - Robot.elevator.getHeight();
-    	Robot.elevator.setSpeed(error * EEConstants.HeightP);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return (Robot.elevator.getHeight()/
-        		EEConstants.TOTES_TO_INCHES == toteheight || elevator.getLimSwitch());
+        return false;
     }
 
     // Called once after isFinished returns true
     protected void end() {
-    	
     }
 
     // Called when another command which requires one or more of the same
