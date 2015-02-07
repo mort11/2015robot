347418b9a92ddedb2fd5b5b7e29c61b11d4c76f7
JeffP <jeffrey.pastilha@motsd.org>
2015-02-07 18:47:11 -0500

    Created vector and angles class

--- src/org/mort11/commands/ee/ElevateToHeight.java
+++ src/org/mort11/commands/ee/ElevateToHeight.java
@@ -17,11 +17,7 @@ public class ElevateToHeight extends Command {
 	Timer time = new Timer();
 	Profiler profiler;
 	boolean useP;
-<<<<<<< HEAD
-	
-=======
 
->>>>>>> deac4b513b6653e4c03b7d7d81756f69e34f6aa6
 	/**
 	 * 
 	 * @param toteHeight
@@ -36,27 +32,6 @@ public class ElevateToHeight extends Command {
 		profiler = new Profiler(1, EEConstants.TIME_PER_LEVEL * toteHeight);
 	}
 
-	// Called just before this Command runs the first time
-	protected void initialize() {
-		Robot.brake.setSolenoid(false);
-		Robot.elevator.resetEnc();
-		time.start();
-		System.out.println("resetting");
-	}
-
-	// Called repeatedly when this Command is scheduled to run
-	protected void execute() {
-		if (useP) {
-			double error = toteheight - Robot.elevator.getHeight();
-			Robot.elevator.setSpeed(error * EEConstants.ELEVATOR_P);
-		} else {
-			// increment virtual sp by deltaV * t
-			double speed = profiler.getDesiredVelocity(time.get());
-			Robot.elevator.setSpeed(speed);
-		}
-	}
-
-<<<<<<< HEAD
     // Called just before this Command runs the first time
     protected void initialize() {
     	eleBrakeOff.initialize();
@@ -88,20 +63,6 @@ public class ElevateToHeight extends Command {
     protected void end() {
     	eleBrake.initialize();
     }
-=======
-	// Make this return true when this Command no longer needs to run execute()
-	protected boolean isFinished() {
-		// epsilon compare on robot height/limitswitch tripped
-		return (Math.abs(Robot.elevator.getHeight() - toteheight) < 0.5 || elevator
-				.getLimSwitch());
-	}
-
-	// Called once after isFinished returns true
-	protected void end() {
-		Robot.brake.setSolenoid(true);
-
-	}
->>>>>>> deac4b513b6653e4c03b7d7d81756f69e34f6aa6
 
 	// Called when another command which requires one or more of the same
 	// subsystems is scheduled to run
