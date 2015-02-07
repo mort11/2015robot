42251d326be51922a4c771aae6e1a4d207c70f74
JeffP <jeffrey.pastilha@motsd.org>
2015-02-07 18:51:52 -0500

    BECAUSE OF MERGE ERRORS

---
+++ src/org/mort11/util/Vector2D.java
@@ -0,0 +1,68 @@
+package org.mort11.util;
+//What's our vector, Victor?
+public class Vector2D {
+	// internal representation 
+	private double angle;
+	private double magnitude;
+	
+	// Create vector from x and y
+	public Vector2D (double xCoord, double yCoord){ 	
+		magnitude = Math.sqrt ((xCoord *xCoord) + (yCoord * yCoord));
+		angle = Math.atan (yCoord/xCoord);
+	}
+	
+	//Create a vector from 2 points, x1 and y1 are subtracted from x2 and y2
+	public Vector2D (double x1, double y1, double x2, double y2){ 	
+		double x = x2 - x1;
+		double y = y2 - y1;
+		new Vector2D (x, y);
+	}
+	
+	//constructor with angle in degrees
+	public Vector2D (Angle angle, double magnitude) {	
+		this.angle = angle.inDegrees();
+		this.magnitude = magnitude;
+	}
+	
+	//scalar multiply, add subtract, rotate (in degrees)
+	public double getMagnitude (){
+		return magnitude;
+	}
+	
+	public double getX (){
+		return Math.cos(angle) * magnitude;
+		
+	}
+
+	public double getY (){
+		return Math.sin(angle) * magnitude;
+	}
+	
+	public double getAngle (){
+		return angle;
+	}
+	
+	public Vector2D scalarMultiply (double scalar){
+		Angle angle = new Angle (this.angle); 
+		Vector2D temp = new Vector2D(angle, this.magnitude*scalar);
+		return temp;
+	}
+	
+	public Vector2D addVectors (Vector2D v1, Vector2D v2){
+		Vector2D addedVector = new Vector2D ((v1.getX() + v2.getX()),
+											 (v1.getY() + v2.getY()));
+		return addedVector;
+	}
+
+	public Vector2D subtractVectors (Vector2D v1, Vector2D v2){
+		Vector2D subtractedVector = new Vector2D ((v1.getX() - v2.getX()),
+												  (v1.getY() - v2.getY()));
+		return subtractedVector;
+	}
+	
+	public Vector2D rotateCW (double addAngle){
+		Angle angle = new Angle (this.angle + addAngle);
+		Vector2D rotatedVector = new Vector2D (angle, magnitude);
+		return rotatedVector;
+	}
+}
